0c65fe8fb2dc5c7f458d3cf2df80a65c7e65f45a
Sam Collins <oscollins@gmail.com>
2020-05-23 07:52:08 -0700

    Fixes dot env parsing

--- api/_db.js
+++ api/_db.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 const serviceAccount = {
   type: "service_account",
   project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
